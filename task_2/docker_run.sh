2722b6fbe32d33e25d84a3f83ece3ab2abb5489d
Marsel_Masyagutov <Marsel_Masyagutov@epam.com>
2022-04-24 21:34:32 +0500

    Change task_2 docker_run.sh

--- task_2/docker_run.sh
+++ task_2/docker_run.sh
@@ -1,2 +1,4 @@
+docker build -t task2 .
+docker network create network
 docker run --name pg --network network -e POSTGRES_PASSWORD=123456 -d postgres:13.3
 docker run --name manage --network network -p 8000:8000 --rm task2
